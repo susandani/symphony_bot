2fb61dadb591848256e9481e8467baf62b9b2602
susandani <162785149+susandani@users.noreply.github.com>
2024-08-14 08:34:26 +0100

    Create header.js

---
+++ header.js
@@ -0,0 +1,25 @@
+export const HEADERS = {
+  // General headers
+  accept: '*/*',
+  'accept-encoding': 'gzip, deflate, br, zstd',
+  'accept-language': 'en-US,en;q=0.7',
+  'cache-control': 'no-cache',
+  origin: 'https://testnet.ping.pub',
+  pragma: 'no-cache',
+  priority: 'u=1, i',
+  referer: 'https://testnet.ping.pub/',
+
+  // Security headers
+  'sec-ch-ua': '"Not)A;Brand";v="99", "Brave";v="127", "Chromium";v="127"',
+  'sec-ch-ua-mobile': '?0',
+  'sec-ch-ua-platform': '"macOS"',
+  'sec-fetch-dest': 'empty',
+  'sec-fetch-mode': 'cors',
+  'sec-fetch-site': 'same-site',
+  'sec-gpc': '1',
+
+  // User agent
+  'user-agent': `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) 
+                AppleWebKit/537.36 (KHTML, like Gecko) 
+                Chrome/127.0.0.0 Safari/537.36`,
+};
